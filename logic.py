a0e9e732c6b4320cf9e147192756762682ac9957
William Hutter <william.hutter1984@gmail.com>
2025-08-01 16:54:02 +0800

    feat: added the basic libraries and constants to start builing some functions

--- logic.py
+++ logic.py
@@ -1,1 +1,9 @@
 import pandas as pd
+from pathlib import Path
+
+#Constants 
+INVENTORY_DIR = Path("test files")
+PRODUCTS_FILE = INVENTORY_DIR / "Products.xlsx"
+
+def load_products_excel():
+    pass
