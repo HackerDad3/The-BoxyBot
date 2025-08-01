17262f2aededc3e305d28f0d18603a4fd5e5312e
William Hutter <william.hutter1984@gmail.com>
2025-08-01 16:54:47 +0800

    chore: updated comments

--- logic.py
+++ logic.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pathlib import Path
 
 #Constants 
-INVENTORY_DIR = Path("test files")
-PRODUCTS_FILE = INVENTORY_DIR / "Products.xlsx"
+INVENTORY_DIR = Path("test files") # to be updated to NAS location for production
+PRODUCTS_FILE = INVENTORY_DIR / "Products.xlsx" # Needs to be changed to what ever file name mum picked
 
 def load_products_excel():
     pass
